da43730feeb4cc4bdea94513d9e48e6a6e77027c
Alexey Melnichuk <mimir@newmail.ru>
2016-04-18 12:20:53 +0300

    Add. Subscribe example.

---
+++ examples/GNTPSubscribe.lua
@@ -0,0 +1,148 @@
+package.path = "../src/lua/?.lua;" .. package.path
+
+local uv   = require "lluv"
+local GNTP = require "gntp"
+local uuid = require "uuid"
+
+local options = {
+  -- self identify on remote growl server
+  name           = 'GNTPSubscriber';
+  sid            = uuid.new();
+
+  -- encryption/auth options
+  pass           = '123456';
+  encrypt        = 'AES';
+  hash           = 'SHA256';
+
+  retry_timeout  = 30;
+
+  -- start local growl server to accept notify
+  listen_host    = '*';
+  listen_port    = '23054';
+
+  -- connect to growl server
+  server_host    = '127.0.0.1';
+  server_port    = '23053';
+}
+
+local function GNTPSubscribe(opt, cb)
+  local msg = GNTP.Message.new('SUBSCRIBE', opt.hash, opt.encrypt)
+    :add_header('Subscriber-ID',   opt.sid)
+    :add_header('Subscriber-Name', opt.sname)
+    :add_header('Subscriber-Port', opt.sport or '23053')
+
+  if opt.ttl then
+    msg:add_header('Subscriber-Port', opt.sport or '23053')
+  end
+
+  uv.tcp():connect(opt.host or "127.0.0.1", opt.port or 23053, function(cli, err)
+    if err then
+      cli:close()
+      return cb(err)
+    end
+
+    local parser = GNTP.Parser.new()
+    cli:start_read(function(cli, err, data)
+      if err then
+        cli:close()
+        return cb(err)
+      end
+
+      parser:append(data)
+      local msg, err = parser:next_message(opt.pass)
+      if msg ~= true then
+        if msg or err then
+          cli:close()
+          return cb(err, msg)
+        end
+      end
+    end)
+
+    cli:write(msg:encode(opt.pass), function(cli, err)
+      if err then
+        cli:close()
+        return cb(err)
+      end
+    end)
+  end)
+end
+
+local function GNTPListen(opt, cb)
+  uv.tcp():bind(opt.host or "127.0.0.1", opt.port or 23053, function(srv, err)
+    if err then
+      print("SERVER BIND ERROR:", err)
+      return uv.stop()
+    end
+
+    srv:listen(function()
+      local cli, err = srv:accept()
+      if cli then
+        local parser = GNTP.Parser.new()
+        cli:start_read(function(cli, err, data)
+          if err then return cli:close() end
+          parser:append(data)
+          local msg, err = parser:next_message(opt.pass)
+          if not msg then
+            print('PARSER ERROR:', err)
+            return cli:close()
+          end
+          if msg == true then return end
+          cb(cli, msg)
+        end)
+      end
+    end)
+  end)
+end
+
+local response = GNTP.Message.new("-OK")
+local function OnMessage(cli, msg)
+  print(msg:encode())
+  local nid = msg:header('Notification-ID')
+
+  response:add_header('Notification-ID', nid)
+  cli:write(response:encode())
+end
+
+GNTPListen({
+  host = options.listen_host, port = options.listen_port,
+  hash = options.hash, encrypt = options.encrypt,
+  pass = options.pass .. options.sid
+}, OnMessage)
+
+uv.timer():start(function(timer)
+  GNTPSubscribe({
+    host = options.server_host, port = options.server_port,
+    sid = options.sid, pass = options.pass, sname = options.name,
+    sport = options.listen_port
+  }, function(err, msg)
+    if err then
+      print("SUBSCRIBE ERROR:", err)
+      return timer:again(options.retry_timeout)
+    end
+
+    if msg:status() == '-ERROR' then
+      print(string.format("Sybscribe error: [%s] %s",
+        msg:header('Error-Code') or '----',
+        msg:header('Error-Description') or '----'
+      ))
+      return timer:again(options.retry_timeout)
+    end
+
+    if msg:status() == '-OK' then
+      local ttl = tonumber(msg:header('Subscription-TTL'))
+      if not ttl then
+        print("WARNING: no `Subscription-TTL` header in response")
+        ttl = 60
+      end
+      local exp = math.max(ttl - 10, math.floor(0.9 * ttl))
+      print(string.format("Subscribe TTL: %d/%d", ttl, exp))
+      return timer:again(exp*1000)
+    end
+
+    print('ERROR: UNKNOWN RESPONSE')
+    print(msg:encode())
+    timer:again(options.retry_timeout)
+  end)
+end)
+
+uv.run()
