13c245b271edf95d52c42fe7dcf7b5ebe43a1cf6
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-05-10 09:44:23 +0300

    Release 0.1.1

--- src/lua/gntp.lua
+++ src/lua/gntp.lua
@@ -15,7 +15,7 @@ local crypto = require "gntp.crypto"
 
 local GNTP = {
   _NAME      = "gntp";
-  _VERSION   = "0.1.1-dev";
+  _VERSION   = "0.1.1";
   _COPYRIGHT = "Copyright (C) 2015-2016 Alexey Melnichuk";
   _LICENSE   = "MIT";
 }
