e52e8bf55095a1dd8ebe058581a000ff4516076b
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2016-05-10 09:54:14 +0300

    Back to develop version. [ci skip]

--- src/lua/gntp.lua
+++ src/lua/gntp.lua
@@ -15,7 +15,7 @@ local crypto = require "gntp.crypto"
 
 local GNTP = {
   _NAME      = "gntp";
-  _VERSION   = "0.1.1";
+  _VERSION   = "0.1.2-dev";
   _COPYRIGHT = "Copyright (C) 2015-2016 Alexey Melnichuk";
   _LICENSE   = "MIT";
 }
